2f47a15a88d40345cae58d80addadcb1b83a593d
ardririy <hynnkgw27@gmail.com>
2024-11-06 20:08:24 +0900

    chore: マークダウンをファイル経由で読み込むように変更

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, process::exit};
+use std::{env, fs::File, io::Read, process::exit};
 use itertools::Itertools;
 
 #[derive(Clone, Copy, Debug)]
@@ -131,11 +131,17 @@ impl Lexer {
 fn main() {
     let args :Vec<String> = env::args().collect();
     if args.len() < 2 {
-        eprintln!("Usage: {} <markdown content>", args[0]);
+        eprintln!("Usage {} <md file path>", args[0]);
         exit(1);
     }
 
-    let mut lexer = Lexer::new(args[1].chars().collect());
+    let mut f = File::open(&args[1])
+        .expect("file not found");
+    let mut content = String::new();
+    f.read_to_string(&mut content)
+        .expect("cannot read file");
+
+    let mut lexer = Lexer::new(content.chars().collect());
     let tokens = lexer.tokenize();
     dbg!(&tokens);
 
